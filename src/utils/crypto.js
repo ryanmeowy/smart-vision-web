54c916ec7b0b82e077546308d4760023615555df
ryan <ryanxys@gmail.com>
2025-12-25 11:34:20 +0800

    chore: Added crypto-js dependency and integrated data decryption function

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,13 +1,12 @@
 import CryptoJS from 'crypto-js'
 
-const RAW_KEY = import.meta.env.VITE_APP_ENCRYPT_KEY || ''
-const RAW_IV = import.meta.env.VITE_APP_ENCRYPT_IV || ''
+const BASE64_KEY = import.meta.env.VITE_APP_ENCRYPT_KEY || ''
+const BASE64_IV = import.meta.env.VITE_APP_ENCRYPT_IV || ''
 
+const KEY = CryptoJS.enc.Base64.parse(BASE64_KEY)
+const IV = CryptoJS.enc.Base64.parse(BASE64_IV)
 
 export function decrypt(word) {
-    const KEY = CryptoJS.enc.Utf8.parse(RAW_KEY) // 16位
-    const IV = CryptoJS.enc.Utf8.parse(RAW_IV)  // 16位
-
     const encryptedHexStr = CryptoJS.enc.Base64.parse(word)
     const srcs = CryptoJS.enc.Base64.stringify(encryptedHexStr)
 
